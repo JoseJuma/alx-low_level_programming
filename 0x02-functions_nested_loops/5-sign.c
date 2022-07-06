e7202ef82e09d2d78ecc9684c394d800908ad521
JoseJuma <geolaro12@gmail.com>
2022-07-06 14:36:03 -0700

    sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
 /**
  * print_sign - prints the sign of a number
  * Description: prints the sign of a number
- * @n: character to check
- *
+ * @n: argument passed
+ * Return: 1, 0, -1
  */
 
 int print_sign(int n)
