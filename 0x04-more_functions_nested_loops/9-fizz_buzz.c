055fd313144d5f9a4d4537b8c59ff85839e89429
JoseJuma <geolaro12@gmail.com>
2022-07-08 18:08:34 -0700

    multiples of 3,5 and both

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Decription: print Fizz if multiplies os 3 ,Buzz of 5 and FizzBuzz for both
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+
+		else if (i % 5 ==)
+			printf("Buzz");
+
+		if (i != 100)
+			printf(" ");
+		i++
+	}
+	printf("\n");
+
+	return (0);
+}
