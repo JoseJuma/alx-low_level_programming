0a11ce8d90630803492a75f79c99dd5804d2ebb5
JoseJuma <geolaro12@gmail.com>
2022-07-08 17:00:57 -0700

    square

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_square - function tha prints a square
+ * @size: size of the square
+ *
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+	y = 0;
+
+
+	if (size < 1)
+	{
+
+		_putchar('\n');
+	}
+
+	while (y < size)
+	{
+		x = 0;
+
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+
+		}
+		_putchar('\n');
+		y++
+	}
+}
