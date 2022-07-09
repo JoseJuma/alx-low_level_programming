2512b24f7998bb6fa49c5026820d1b2cbda7d154
JoseJuma <geolaro12@gmail.com>
2022-07-08 18:33:46 -0700

    largest prime number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,17 @@
 
 void more_numbers(void)
 {
-	int i;
-	int h;
+	int x, y;
 
-	for (h = 0; h <= 10; h++)
+	for (x = 0; x <= 10; x++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (i > 9)
+			if (y > 9)
 			{
-				_putchar(i / 10 + '0');
+				_putchar(y / 10 + '0');
 			}
-			-putchar(i % 10 + '0');
+			-putchar(y % 10 + '0');
 		}
 		_putchar('\n');
 	}
