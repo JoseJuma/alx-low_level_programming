7e0cd306c0c84f249fe24e93553495b055524318
JoseJuma <geolaro12@gmail.com>
2022-07-11 14:53:19 -0700

    prints char of a string one after the other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other char of a string
+ * @str: char array 
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
