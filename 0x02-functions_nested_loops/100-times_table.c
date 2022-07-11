04345e5fa352842b048329f3fe604173f3268dd7
JoseJuma <geolaro12@gmail.com>
2022-07-11 13:29:18 -0700

    swap

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * print_times_table - it will print the times table of an input
+ * @n: int
+ */
+void pr
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function swaps the value of two integers
+ * @a: int type
+ * @b: inttype
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b
+	*b = c;
+}
