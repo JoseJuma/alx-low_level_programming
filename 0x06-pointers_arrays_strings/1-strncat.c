5da545df99d19cc0367cbcdb0557cf9d3ff2a9d7
JoseJuma <geolaro12@gmail.com>
2022-07-15 09:28:01 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * *_strcat - concatenates two strings
+ * _strcat - concatenates two strings
  * @dest: pointer to the string to be joined
  * @src: source string to be appended
  * Return: pointer to dest
  *
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src)
 {
-	int index = 0, dest_len - 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
